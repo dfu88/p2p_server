001a2b254988aec339cdeffc9e86caa3c2f60a96
Dylan Fu <dylan.fu@hotmail.co.nz>
2018-06-02 22:38:01 +1200

    Added getter and setter db functions for profile data

--- Db.py
+++ Db.py
@@ -29,18 +29,18 @@ def createDb(userList):
 	c = conn.cursor()
 	try:
 		c.execute('''CREATE TABLE IF NOT EXISTS Users (id integer PRIMARY KEY, username text, ip text, location text, lastLogin text, port text, publicKey text)''')
-		c.execute('''CREATE TABLE IF NOT EXISTS Profiles (id integer PRIMARY KEY, username text, fullname text, position text, description text, location text, picture text, encoding text, encryption text, decryptionKey text, status text, secretKey text)''')
+		c.execute('''CREATE TABLE IF NOT EXISTS Profiles (id integer PRIMARY KEY, username text, fullname text, position text, description text, location text, imgURL text, encoding text, encryption text, decryptionKey text, status text, secretKey text)''')
 		c.execute('''CREATE TABLE IF NOT EXISTS Messages_Files(id integer PRIMARY KEY, sender text, destination text, message text, stamp text, encoding text, encryption text, hashing text, hash text, decryptionKey text, file text, filename, content_type text, messageStatus text)''')
 		initUserData(userList)
 		c.close()
 		conn.commit()
 		conn.close()
-		print "createDb success"
+		# print "createDb success"
 		# return 1
 	except:
 		c.close()
 		conn.close()
-		print "createDb falied"
+		# print "createDb falied"
 		# return 0;
 	
 
@@ -59,13 +59,13 @@ def initUserData(userList):
 		c.close()
 		conn.commit()
 		conn.close()
-		print "initUserData success"
+		# print "initUserData success"
 	except:
-		print "initUserData failed"
+		# print "initUserData failed"
 		pass
 
 def updateUserData(data):
-	print "trying to update userdata"
+	# print "trying to update userdata"
 	try:
 		conn = sqlite3.connect(DB_NAME)
 		c = conn.cursor()
@@ -108,39 +108,70 @@ def getAllUserDataAsList():
 def getUserDataAsList(destination):
 	conn = sqlite3.connect(DB_NAME)
 	c = conn.cursor()
-	c.execute("SELECT * FROM Users WHERE username='{a}'".format(a=destination))
+	c.execute("SELECT * FROM Users WHERE username=?", (destination,))
 	userData = [dict(zip(['rowid', 'username', 'ip', 'location', 'lastLogin', 'port', 'publicKey'], row)) for row in c.fetchall()]
 	conn.commit()
 	conn.close()
 	return userData
 
-def getMessages(destination,sender):
-	conn = sqlite3.connect(DB_NAME)
-	c = conn.cursor()
-	c.execute("SELECT * FROM Messages_Files WHERE sender='{a}' AND destination='{b}' OR sender='{b}' AND destination='{a}'".format(a=destination, b=sender))
-	messageList = [dict(zip(['sender', 'destination', 'message', 'stamp', 'encoding', 'encryption','hashing', 'hash', 'decryptionKey','messageStatus'], row)) for row in c.fetchall()]
-	c.close()
-	conn.commit()
-	conn.close()
-	return messageList
+def saveProfile(dictionary, username):
+    """
+    Saves new data to a users profile.
+    """
+    if isinstance(dictionary, unicode):
+        dictionary = ast.literal_eval(dictionary)
+    conn = sqlite3.connect(DB_NAME)
+    # Create a query cursor on the db connection
+    c = conn.cursor()
+    c.execute("SELECT rowid FROM Profiles WHERE username = ?", (username,))
+    data = c.fetchone()
+    if data == None:
+        c.execute('''INSERT INTO Profiles (username, fullname, position, description, location, imgURL) VALUES (?,?,?,?,?,?)''', (username, dictionary.get('fullname'), dictionary.get('position'), dictionary.get('description'), dictionary.get('location'), dictionary.get('imgURL')))
+    else:
+        c.execute('''UPDATE Profiles SET username=?, fullname=?, position=?, description=?, location=?, imgURL=? WHERE rowid=?''', [username, dictionary.get('fullname'), dictionary.get('position'), dictionary.get('description'), dictionary.get('location'), dictionary.get('imgURL'), data[0]])
+    c.close()
+    conn.commit()
+    conn.close()
+    return True
+
+def getProfile(username):
+    conn = sqlite3.connect(DB_NAME)
+    # Create a query cursor on the db connection
+    c = conn.cursor()
+    c.execute("SELECT * FROM Profiles WHERE username = '{a}'".format(a=username))
+    userProfileData = [dict(zip(['rowid', 'username', 'fullname', 'position', 'description', 'location', 'imgURL', 'encoding', 'encryption', 'decryptionKey', 'status', 'secretKey'], row)) for row in c.fetchall()]
+    c.close()
+    conn.commit()
+    conn.close()
+    return userProfileData
 
 def saveMessage(dictionary):
 	print " INSIDE DB"
 	conn = sqlite3.connect(DB_NAME)
 	c = conn.cursor()
-	print "ISIDE ISH"
+	# print "ISIDE ISH"
 	if dictionary.get('messageStatus') == None:
 		dictionary['messageStatus'] = "Receive Unconfirmed"
 	
 	# c.execute('''INSERT INTO Messages_Files''')
 	c.execute("SELECT rowid FROM Messages_Files WHERE sender=? and destination=? and stamp=? and hash=?", (dictionary.get('sender'), dictionary.get('destination'), dictionary.get('stamp'), dictionary.get('hash'),))
 	data = c.fetchone()
-	print "3193878749"
+	# print "3193878749"
 	if data == None:
 		c.execute('''INSERT INTO Messages_Files(sender, destination, message, stamp, encoding, encryption, hashing, hash, decryptionKey, file, filename, content_type, messageStatus) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)''', (dictionary.get('sender'), dictionary.get('destination'), dictionary.get('message'), dictionary.get('stamp'), dictionary.get('encoding'), dictionary.get('encryption'), dictionary.get('hashing'), dictionary.get('hash'), dictionary.get('decryptionKey'), dictionary.get('file'), dictionary.get('filename'), dictionary.get('content_type'), dictionary.get('messageStatus')))
-		print "487989"
-	print "YOOOOOOOOOO"
+		# print "487989"
+	# print "YOOOOOOOOOO"
 	c.close()
 	conn.commit()
 	conn.close()
 	return True
+
+def getMessages(destination,sender):
+	conn = sqlite3.connect(DB_NAME)
+	c = conn.cursor()
+	c.execute("SELECT * FROM Messages_Files WHERE sender='{a}' AND destination='{b}' OR sender='{b}' AND destination='{a}'".format(a=destination, b=sender))
+	messageList = [dict(zip(['rowid', 'sender', 'destination', 'message', 'stamp', 'encoding', 'encryption','hashing', 'hash', 'decryptionKey','messageStatus'], row)) for row in c.fetchall()]
+	c.close()
+	conn.commit()
+	conn.close()
+	return messageList
