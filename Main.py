d3cceb60eb70bef22b6c9893b995157af54fa145
Dylan Fu <dylan.fu@hotmail.co.nz>
2018-06-02 22:42:35 +1200

    Added more functions for html ajax calls

--- Main.py
+++ Main.py
@@ -48,9 +48,9 @@ class MainApp(object):
             userList = self.userListServer()
             print userList
             Db.createDb(userList)
-            print "init db"
+            # print "init db"
         except:
-            print "did not init db"
+            # print "did not init db"
             pass
         finally:
             self.loggedIn = False
@@ -59,7 +59,6 @@ class MainApp(object):
             self.timer = Timer(60, self.reportServer)
             self.timer.start()
             self.timer.pause()
-        pass
 
     # If they try somewhere we don't know, catch it here and send them to the right place.
     """The default page, given when we don't recognise where the request is for."""
@@ -74,6 +73,8 @@ class MainApp(object):
     @cherrypy.expose
     def index(self,username=None):
         if self.loggedIn:
+            cherrypy.session['viewUserProfile'] = None
+            cherrypy.session['viewUserMessage'] = None
             Page = file('index.html')
             return Page
         else:
@@ -111,19 +112,29 @@ class MainApp(object):
             raise cherrypy.HTTPRedirect('/')
 
     @cherrypy.expose
-    def sendMessage(self, message, destination='dfu987', sender=None, attachments=None):
-        if sender == None:
-            sender = self.username
-        # if destination == None
-        #     destination = self.username
+    def message(self, username=None):
+        if username != None:
+            cherrypy.session['viewUserMessage'] = username
+        else:
+            cherrypy.session['viewUserMessage'] = self.username
+        Page = file('message.html')
+        return Page
+
+    @cherrypy.expose
+    def sendMessage(self, message, attachments=None):
+        sender = self.username
+        destination = cherrypy.session['viewUserMessage']
         payload = {'sender': sender, 'destination': destination, 'message': message, 'stamp': round(float(time.time()))}
         #Need to handle hashing and encryption later
+        print "1231414"
         userData = Db.getUserDataAsList(destination)
+        print userData
+        print "sucesdjkanjvavnjkdaa"
         request = None
         if sender == destination:
             request = urllib2.Request('http://0.0.0.0:' + str(listen_port) + '/receiveMessage' , self.encJSON(payload), {'Content-Type': 'application/json'})
         else:
-            request = urllib2.Request('http://' + userData[0]['ip'] +':' + userData[0]['port'] + '/receiveMessage' , self.JSON_encode(payload), {'Content-Type': 'application/json'})
+            request = urllib2.Request('http://' + userData[0]['ip'] +':' + userData[0]['port'] + '/receiveMessage' , self.encJSON(payload), {'Content-Type': 'application/json'})
         response = urllib2.urlopen(request).read()
 
         #Check if message received
@@ -131,6 +142,7 @@ class MainApp(object):
             Db.saveMessage(payload)
         elif '0' not in response:
             payload['message'] = "Message Send Failed: " + response
+        raise cherrypy.HTTPRedirect("message/?username="+cherrypy.session['viewUserMessage'])
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
@@ -147,19 +159,47 @@ class MainApp(object):
             # if (unicode(dictionary['enc']) and 'encryption' in dictionary) != u'0':
             #     return "9: Encryption Standard Not Supported"
             Db.saveMessage(dictionary)
-            print "!!!!!!!!testing"
+            # print "!!!!!!!!testing"
             return '0: Message Received'
         except Exception as e:
             return '-1: Internal Error'
 
+    @cherrypy.expose
+    def viewProfile(self, username=None):
+        if username != None:
+            cherrypy.session['viewUserProfile'] = username
+        else:
+            cherrypy.session['viewUserProfile'] = self.username
+        Page = file('viewProfile.html')
+        return Page
 
     @cherrypy.expose
     def editProfile(self,fullname=None,position=None,description=None,location=None,imgURL=None):
         if self.loggedIn:
-            return file('editProfile.html')
+            if fullname != None or position != None or description != None or location != None or imgURL != None:
+                raise cherrypy.HTTPRedirect("/")    
+            else:
+                return file('editProfile.html')
         else:
             raise cherrypy.HTTPRedirect("/")
 
+    @cherrypy.expose
+    def updateProfile(self,fullname=None,position=None,description=None,location=None,imgURL=None):
+        data = {'fullname': fullname, 'position': position, 'description': description, 'location': location, 'imgURL': imgURL}
+        Db.saveProfile(data, self.username)
+        raise cherrypy.HTTPRedirect("/")
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def getProfile(self):
+        dictionary = cherrypy.request.json
+        if 'profile_username' not in dictionary or 'sender' not in dictionary:
+                return ('1: Missing compulsory Field')
+        data = Db.getProfile(dictionary['profile_username'])
+        if data == None:
+            return ('4: Profile Not Found')
+        else:
+            return data
 
     @cherrypy.expose
     def ping(self, sender=None):
@@ -211,15 +251,13 @@ class MainApp(object):
             lastLogin = time.strftime(
                 "%Y/%m/%d, %H:%M:%S", time.localtime(float(user['lastLogin'] or 0)))
             if int(user['lastLogin'] or 0) + 86400 > int(time.time()):
-                user['lastLogin'] = time.strftime(
-                    "%H:%M:%S", time.localtime(float(user['lastLogin'] or 0)))
+                user['lastLogin'] = time.strftime("%H:%M:%S", time.localtime(float(user['lastLogin'] or 0)))
             elif user['lastLogin'] == None:
                 user['lastLogin'] = 'NEVER'
             elif int(user['lastLogin'] or 0) == 0:
                 user['lastLogin'] = 'NEVER'
             else:
-                user['lastLogin'] = time.strftime(
-                    "%a, %d %b %Y", time.localtime(float(user['lastLogin'] or 0)))
+                user['lastLogin'] = time.strftime("%a, %d %b %Y", time.localtime(float(user['lastLogin'] or 0)))
 
         dataList = {str(k): v for k, v in enumerate(data)}
         return dataList
@@ -227,18 +265,27 @@ class MainApp(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def getMessageListJSON(self):
-        username = cherrypy.serving.request.headers['Referer']
-        print username
-        messageList = Db.getMessages(username, self.username)
+        # username = cherrypy.serving.request.headers['Referer']
+        # print username
+        messageList = Db.getMessages(cherrypy.session['viewUserMessage'], self.username)
         for row in messageList:
-            row['stamp'] = time.strftime(
-                "%Y/%m/%d, %H:%M:%S", time.localtime(float(row['stamp'])))
+            row['stamp'] = time.strftime("%Y/%m/%d, %H:%M:%S", time.localtime(float(row['stamp'] or 0)))
             row['message'] = string.replace(row['message'], "''", "'")
             row['username'] = row['sender']
-        
+
         somelist = {str(k): v for k, v in enumerate(messageList)}
-        print somelist
+        # print somelist
         return somelist
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def getUserProfileJSON(self):
+        data = Db.getProfile(cherrypy.session['viewUserProfile'])
+        if data == None:
+            return ('4: Profile Not Found')
+        else:
+            finalData = {str(k): v for k, v in enumerate(data)}
+            return finalData
 
     ####### LOGIN SERVER ########
     def reportServer(self,username=None,password=None):
