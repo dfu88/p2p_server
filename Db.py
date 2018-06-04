8e13e06e52eed02e8dd261b72aae54d83c57ad60
Dylan Fu <dylan.fu@hotmail.co.nz>
2018-06-05 01:04:51 +1200

    Changed db columns

--- Db.py
+++ Db.py
@@ -28,10 +28,11 @@ def createDb(userList):
 	conn = sqlite3.connect(DB_NAME)
 	c = conn.cursor()
 	try:
-		c.execute('''CREATE TABLE IF NOT EXISTS Users (id integer PRIMARY KEY, username text, ip text, location text, lastLogin text, port text, publicKey text)''')
-		c.execute('''CREATE TABLE IF NOT EXISTS Profiles (id integer PRIMARY KEY, username text, fullname text, position text, description text, location text, imgURL text, encoding text, encryption text, decryptionKey text, status text, secretKey text)''')
-		c.execute('''CREATE TABLE IF NOT EXISTS Messages_Files(id integer PRIMARY KEY, sender text, destination text, message text, stamp text, encoding text, encryption text, hashing text, hash text, decryptionKey text, file text, filename, content_type text, messageStatus text)''')
+		c.execute('''CREATE TABLE IF NOT EXISTS Users (id integer PRIMARY KEY, username text, ip text, location text, lastLogin text, port text, publicKey text, picture text)''')
+		c.execute('''CREATE TABLE IF NOT EXISTS Profiles (id integer PRIMARY KEY, username text, fullname text, position text, description text, location text, picture text, encoding text, encryption text, decryptionKey text, secretKey text, lastUpdated text)''')
+		c.execute('''CREATE TABLE IF NOT EXISTS Messages_Files(id integer PRIMARY KEY, sender text, destination text, message text, stamp text, encoding text, encryption text, hashing text, hash text, decryptionKey text, messageStatus text)''')
 		initUserData(userList)
+		initUserProfile(userList)
 		c.close()
 		conn.commit()
 		conn.close()
@@ -55,13 +56,30 @@ def initUserData(userList):
 			c.execute("SELECT rowid FROM Users WHERE username = ?",(username,))
 			data = c.fetchone()
 			if data is None:
-				c.execute('''INSERT INTO Users(username, ip, location, lastLogin, port, publicKey) VALUES (?,?,?,?,?,?)''',(username,"","","","",""))
+				c.execute('''INSERT INTO Users(username, ip, location, lastLogin, port, publicKey, picture) VALUES (?,?,?,?,?,?,?)''',(username,"","","","","","/static/placeholder.png"))
 		c.close()
 		conn.commit()
 		conn.close()
 		# print "initUserData success"
 	except:
 		# print "initUserData failed"
+		pass
+
+def initUserProfile(userList):
+	try:
+		conn = sqlite3.connect(DB_NAME)
+		c = conn.cursor()
+		for username in userList:
+			c.execute("SELECT rowid FROM Profiles WHERE username = ?",(username,))
+			data = c.fetchone()
+			if data is None:
+				c.execute('''INSERT INTO Profiles(username, fullname, position, description, location, picture, encoding, encryption, decryptionKey, secretKey, lastUpdated) VALUES (?,?,?,?,?,?,?,?,?,?,?)''',(username,"N/A","N/A","N/A","N/A","/static/placeholder.png","","","","",""))
+		c.close()
+		conn.commit()
+		conn.close()
+		# print "initUserProfile success"
+	except:
+		# print "initUserProfile failed"
 		pass
 
 def updateUserData(data):
@@ -100,7 +118,7 @@ def getAllUserDataAsList():
 	conn = sqlite3.connect(DB_NAME)
 	c = conn.cursor()
 	c.execute('SELECT * FROM Users')
-	allUserData = [dict(zip(['rowid', 'username', 'ip', 'location', 'lastLogin', 'port', 'publicKey'], row)) for row in c.fetchall()]
+	allUserData = [dict(zip(['rowid', 'username', 'ip', 'location', 'lastLogin', 'port', 'publicKey', 'picture'], row)) for row in c.fetchall()]
 	conn.commit()
 	conn.close()
 	return allUserData
@@ -109,44 +127,64 @@ def getUserDataAsList(destination):
 	conn = sqlite3.connect(DB_NAME)
 	c = conn.cursor()
 	c.execute("SELECT * FROM Users WHERE username=?", (destination,))
-	userData = [dict(zip(['rowid', 'username', 'ip', 'location', 'lastLogin', 'port', 'publicKey'], row)) for row in c.fetchall()]
+	userData = [dict(zip(['rowid', 'username', 'ip', 'location', 'lastLogin', 'port', 'publicKey', 'picture'], row)) for row in c.fetchall()]
 	conn.commit()
 	conn.close()
 	return userData
 
 def saveProfile(dictionary, username):
-    """
-    Saves new data to a users profile.
-    """
-    if isinstance(dictionary, unicode):
-        dictionary = ast.literal_eval(dictionary)
-    conn = sqlite3.connect(DB_NAME)
-    # Create a query cursor on the db connection
-    c = conn.cursor()
-    c.execute("SELECT rowid FROM Profiles WHERE username = ?", (username,))
-    data = c.fetchone()
-    if data == None:
-        c.execute('''INSERT INTO Profiles (username, fullname, position, description, location, imgURL) VALUES (?,?,?,?,?,?)''', (username, dictionary.get('fullname'), dictionary.get('position'), dictionary.get('description'), dictionary.get('location'), dictionary.get('imgURL')))
-    else:
-        c.execute('''UPDATE Profiles SET username=?, fullname=?, position=?, description=?, location=?, imgURL=? WHERE rowid=?''', [username, dictionary.get('fullname'), dictionary.get('position'), dictionary.get('description'), dictionary.get('location'), dictionary.get('imgURL'), data[0]])
-    c.close()
-    conn.commit()
-    conn.close()
-    return True
+	"""
+	Saves new data to a users profile.
+	"""
+	if dictionary['picture'] is None:
+		dictionary['picture'] = "/static/placeholder.png"
+	if isinstance(dictionary, unicode):
+		dictionary = ast.literal_eval(dictionary)
+	conn = sqlite3.connect(DB_NAME)
+	# Create a query cursor on the db connection
+	c = conn.cursor()
+	c.execute("SELECT rowid FROM Profiles WHERE username = ?", (username,))
+	data = c.fetchone()
+	if data == None:
+	    c.execute('''INSERT INTO Profiles (username, fullname, position, description, location, picture) VALUES (?,?,?,?,?,?)''', (username, dictionary.get('fullname'), dictionary.get('position'), dictionary.get('description'), dictionary.get('location'), dictionary.get('picture')))
+	else:
+	    c.execute('''UPDATE Profiles SET username=?, fullname=?, position=?, description=?, location=?, picture=? WHERE rowid=?''', [username, dictionary.get('fullname'), dictionary.get('position'), dictionary.get('description'), dictionary.get('location'), dictionary.get('picture'), data[0]])
+	c.close()
+	conn.commit()
+	conn.close()
+
+	conn = sqlite3.connect(DB_NAME)
+	c = conn.cursor()
+	c.execute("SELECT rowid FROM Users WHERE username = ?", (username,))
+	data = c.fetchone()
+	c.execute('''UPDATE Users SET username=?, picture=? WHERE rowid=?''', [username, dictionary.get('picture'), data[0]])
+	c.close()
+	conn.commit()
+	conn.close()
+	return True
 
 def getProfile(username):
+	conn = sqlite3.connect(DB_NAME)
+	c = conn.cursor()
+	c.execute("SELECT * FROM Profiles WHERE username=?", (username,))
+	data = c.fetchone()
+	c.close()
+	conn.close()
+	return data
+
+def getProfileAsList(username):
     conn = sqlite3.connect(DB_NAME)
     # Create a query cursor on the db connection
     c = conn.cursor()
     c.execute("SELECT * FROM Profiles WHERE username = '{a}'".format(a=username))
-    userProfileData = [dict(zip(['rowid', 'username', 'fullname', 'position', 'description', 'location', 'imgURL', 'encoding', 'encryption', 'decryptionKey', 'status', 'secretKey'], row)) for row in c.fetchall()]
+    userProfileData = [dict(zip(['rowid', 'username', 'fullname', 'position', 'description', 'location', 'picture', 'encoding', 'encryption', 'decryptionKey', 'status', 'secretKey'], row)) for row in c.fetchall()]
     c.close()
     conn.commit()
     conn.close()
     return userProfileData
 
 def saveMessage(dictionary):
-	print " INSIDE DB"
+	# print " INSIDE DB"
 	conn = sqlite3.connect(DB_NAME)
 	c = conn.cursor()
 	# print "ISIDE ISH"
@@ -156,11 +194,11 @@ def saveMessage(dictionary):
 	# c.execute('''INSERT INTO Messages_Files''')
 	c.execute("SELECT rowid FROM Messages_Files WHERE sender=? and destination=? and stamp=? and hash=?", (dictionary.get('sender'), dictionary.get('destination'), dictionary.get('stamp'), dictionary.get('hash'),))
 	data = c.fetchone()
-	print "3193878749"
+	# print "3193878749"
 	if data == None:
-		c.execute('''INSERT INTO Messages_Files(sender, destination, message, stamp, encoding, encryption, hashing, hash, decryptionKey, file, filename, content_type, messageStatus) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)''', (dictionary.get('sender'), dictionary.get('destination'), dictionary.get('message'), dictionary.get('stamp'), dictionary.get('encoding'), dictionary.get('encryption'), dictionary.get('hashing'), dictionary.get('hash'), dictionary.get('decryptionKey'), dictionary.get('file'), dictionary.get('filename'), dictionary.get('content_type'), dictionary.get('messageStatus')))
-		print "487989"
-	print "YOOOOOOOOOO"
+		c.execute('''INSERT INTO Messages_Files(sender, destination, message, stamp, encoding, encryption, hashing, hash, decryptionKey, messageStatus) VALUES (?,?,?,?,?,?,?,?,?,?)''', (dictionary.get('sender'), dictionary.get('destination'), dictionary.get('message'), dictionary.get('stamp'), dictionary.get('encoding'), dictionary.get('encryption'), dictionary.get('hashing'), dictionary.get('hash'), dictionary.get('decryptionKey'), dictionary.get('messageStatus')))
+		# print "487989"
+	# print "YOOOOOOOOOO"
 	c.close()
 	conn.commit()
 	conn.close()
